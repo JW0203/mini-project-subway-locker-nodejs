1943006c977edff72b911b84a2b5c5abf13581c6
JW0203 <widwoo0203@gmail.com>
2023-11-30 02:22:35 +0900

    feat: running server

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swaggerDef');
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+//app.use(express.json);
+
+app.get('/', (req, res) => {
+    res.send( "<< 네이버 지도앱  or login 화면>>");
+})
+app.listen(port, () =>{
+    console.log(`서버가 실행됩니다. http://localhost:${port}`);
+})
