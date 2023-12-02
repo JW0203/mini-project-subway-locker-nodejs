f954485af56e287643717c444d251adfa0fe54ef
JW0203 <widwoo0203@gmail.com>
2023-12-02 23:46:49 +0900

    feat: add searching post using user email

--- routes/post.js
+++ routes/post.js
@@ -48,10 +48,32 @@ const router = express.Router();
  *                 type:string
  *               content:
  *                 type: string
- *
- *
  */
 
+
+/**
+ * @swagger
+ * /post/{email}:
+ *   get:
+ *     summary: 한 유저가 게시한 포스트 검색
+ *     parameters:
+ *       - in: path
+ *         name: user email
+ *         schema:
+ *             type: string
+ *         required: true
+ *         description: 이메일 포멧  example@email.com
+ *     responses:
+ *       200:
+ *         description: 유저의 게시물 찾기 성공
+ *         application/json:
+ *           schema:
+ *             properties:
+ *               title:
+ *                 type: string
+ *               content:
+ *                 type: string
+ */
 
 // 게시물 게시
 router.post('/', async (req, res) => {
@@ -64,7 +86,11 @@ router.get('/', async (req, res) =>{
 	res.status(200).send("found all post")
 })
 
-//
+// 유저 아이디로 게시물 검색
+router.get('/:email', async(req, res) =>{
+	const userEmail = req.params.email;
+	res.status(200).send("found a user's post.")
+})
 
 
 module.exports = router;
