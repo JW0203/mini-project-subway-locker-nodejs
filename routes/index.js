677dffc5e7b7070a4d2e1aca7f57764f927b3c0f
JW0203 <widwoo0203@gmail.com>
2023-12-02 22:58:29 +0900

    feat: add post route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const lockerRouter = require('./lockers');
 const authRouter = require('./auth');
 const mapRouter = require('./map');
+const postRouter = require('./post');
 
 
 
 module.exports = {
     lockerRouter,
     authRouter,
-    mapRouter
+    mapRouter,
+    postRouter
 }
