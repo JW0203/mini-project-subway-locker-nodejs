ececc08b461a9bf992f2015e7f0c60db5b2f6906
JW0203 <widwoo0203@gmail.com>
2023-12-05 01:28:32 +0900

    feat: add comment

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const app = express();
 const port = 3000;
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swaggerDef');
-const {lockerRouter, authRouter, mapRouter, postRouter} = require('./routes');
+const {lockerRouter, authRouter, mapRouter, postRouter, commentRouter} = require('./routes');
 
+app.use(express.json);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/lockers', lockerRouter);
 app.use('/auth', authRouter);
 app.use('/map', mapRouter);
 app.use('/posts', postRouter);
+app.use('/comments', commentRouter);
 
 app.get('/', (req, res) => {
     res.send( '<< 네이버 지도앱  or login 화면>>');
