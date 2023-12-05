94e8bb3bf45795839f9cd3572d2d311bd6ddfc3d
JW0203 <widwoo0203@gmail.com>
2023-12-05 16:57:12 +0900

    feat: middelware to handle next()

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swaggerDef');
 const {lockerRouter, authRouter, mapRouter, postRouter, commentRouter} = require('./routes');
+const HttpException = require('./middleware/HttpException');
 
 app.use(express.json);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
@@ -16,6 +17,17 @@ app.use('/comments', commentRouter);
 app.get('/', (req, res) => {
     res.send( '<< 네이버 지도앱  or login 화면>>');
 })
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    if (err instanceof HttpException){
+        res.status(err.status).send(err.message);
+    }
+    res.status(500).send({
+        message: "Internal error occurred while processing"
+    })
+})
+
 app.listen(port, () =>{
     console.log(`서버가 실행됩니다. http://localhost:${port}`);
 })
