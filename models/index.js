3b181c239e72dd323a4fc1c026264cef236395ed
JW0203 <widwoo0203@gmail.com>
2023-12-05 16:27:35 +0900

    feat: make asscociation between Message and Comment

--- models/index.js
+++ models/index.js
@@ -5,8 +5,8 @@ const Message = require('./Message');
 const Comment = require('./Comment');
 
 // user : message = 1: N
-User.hasMany(Message, {foreignKey: 'userPk'});
-Message.belongsTo(User, {foreignKey: 'userPk'});
+User.hasMany(Message, {foreignKey: 'userId'});
+Message.belongsTo(User, {foreignKey: 'userId'});
 
 // Message : comment = 1 : 1
 // Comment 에 message_id 라는 컬럼에 해당 Message 의 id 를 추가
