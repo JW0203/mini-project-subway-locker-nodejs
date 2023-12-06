876bcd990d36c468e5d8ecdb0b7e925ae659800c
JW0203 <widwoo0203@gmail.com>
2023-12-06 09:05:56 +0900

    fix: infinite loading issue: express.json -> express.json()

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const swaggerSpec = require('./config/swaggerDef');
 const {lockerRouter, authRouter, mapRouter, postRouter, commentRouter} = require('./routes');
 const HttpException = require('./middleware/HttpException');
 
-app.use(express.json);
+app.use(express.json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/lockers', lockerRouter);
 app.use('/auth', authRouter);
