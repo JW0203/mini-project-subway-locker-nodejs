cf76db11003ce1d733a606f755a8ca528d701ad1
JW0203 <widwoo0203@gmail.com>
2023-12-12 02:40:34 +0900

    feat: posting title and content

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,70 @@
-const sequelize = require('config/database');
+const sequelize = require('../config/database');
 const express = require('express')
 const router = express.Router();
+const {Post, User} = require('../models');
+const HttpException = require('../middleware/HttpException');
 
 
+/**
+ * @swagger
+ * /posts:
+ *   post:
+ *     summary: 문의사항 게시
+ *     requestBody:
+ *       description: 문의 사항을 게시하기위한 제목과 내용
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: 유저 로그인 여부 확인용
+ *               title:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: 문의사항 성공적으로 게시
+ *         content:
+ *           application.json:
+ *             schema:
+ *               properties:
+ *                 title:
+ *                   type: string
+ *                   description: 게시된 게시물 제목
+ *                 content:
+ *                   type: string
+ *                   description: 게시된 게시물 내용
+ *                 userId:
+ *                   type: Integer
+ */
 
+router.post('/', async (req, res, next) => {
+	try{
+		const {email, title, content} = req.body;
+		const user  = await User.findOne({
+			where:{email}
+		})
+		if (!user){
+			throw new HttpException(400, "해당하는 이메일은 등록되어 있지 않습니다."); // 오류
+			return;
+		}
 
+		await sequelize.transaction(async () => {
+			const userId = user.id;
+			const newPost = await Post.create({
+				title,
+				content,
+				userId
+			});
+			res.status(201).send(newPost);
+		})
+	}catch(err){
+		next(err);
+	}
+})
 
 
 module.exports = router;
