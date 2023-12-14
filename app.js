a64e1ed9a1e5071d14b2d17a996023922b2cf691
JW0203 <widwoo0203@gmail.com>
2023-12-14 19:41:05 +0900

    feat: add comments router

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const swaggerSpec = require('./config/swaggerDef');
 const HttpException = require('./middleware/HttpException');
 const {lockerRouter, authRouter,
     mapRouter, userRouter,
-    postsRouter} = require('./routes');
+    postsRouter, commentsRouter
+} = require('./routes');
 
 //sequelize.sync({force:true});
 app.use(express.json());
@@ -17,6 +18,7 @@ app.use('/auth', authRouter);
 app.use('/map', mapRouter);
 app.use('/users', userRouter);
 app.use('/posts', postsRouter);
+app.use('/comments', commentsRouter);
 
 app.get('/', (req, res) => {
     res.send( '<< 네이버 지도앱  or login 화면>>');
