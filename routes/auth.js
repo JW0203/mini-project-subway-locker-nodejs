3f190363669214e135d20645ab73f678e4b5870d
JW0203 <widwoo0203@gmail.com>
2023-12-30 03:40:41 +0900

    feat: add code to save token

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { User } = require('../models');
+const { User, BlackList } = require('../models');
 const sequelize = require('../config/database');
 const express = require('express');
 const router = express.Router();
@@ -164,20 +164,35 @@ router.post('/sign-in', async (req, res, next) => {
       }
 
       const accessToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY, {
-        expiresIn: '1w',
+        expiresIn: '1d',
       });
-      res.cookie('token', accessToken).status(200).send('로그인에 성공하였습니다.');
+
+      const ondDayTimeStamp = 24 * 60 * 60 * 1000;
+      const expiryDate = Date.now() + ondDayTimeStamp;
+      await BlackList.create({
+        refreshedToken: accessToken,
+        expiryDate: expiryDate
+      })
+
+      localStorage.setItem("access_token", accessToken)
+
+      res.status(200).send('로그인에 성공하였습니다.');
     });
   } catch (err) {
     next(err);
   }
 });
 
-router.get('/sign-out', authenticateToken, async (req, res, next) => {
+router.patch('/sign-out', authenticateToken, async (req, res, next) => {
   const id = req.user.id;
   const user = await User.findByPk(id);
   const userEmail = user.email;
-  console.log(user);
+
+  await BlackList.update(
+    {where: {'refreshedToken': token}},
+    {
+      token
+  })
 
   res.cookie('token', '').status(200).send(`${userEmail} 님 로그 아웃 되었습니다.`);
 });
