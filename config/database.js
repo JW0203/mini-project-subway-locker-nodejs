7e0fb6fb87808fc311d1557bf34dc019cb7cba0a
JW0203 <widwoo0203@gmail.com>
2024-03-11 15:28:45 +0900

    fix: revise the db host address

--- config/database.js
+++ config/database.js
@@ -5,7 +5,6 @@ const namespace = cls.createNamespace('jwt-encryption');
 Sequelize.useCLS(namespace);
 
 const sequelize = new Sequelize('ddota', process.env.DB_USER, process.env.DB_PASSWORD, {
-  //host: '127.0.0.1',
   host: process.env.DB_HOST,
   dialect: 'mysql',
   logQueryParameters: true,
